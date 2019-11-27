07c295ca5389aeaa1f701897174e574530a4143a
Austin Helmholz <austin.helmholz@gmail.com>
2019-11-27 15:18:26 -0800

    Removed dead code and made home video span the screen

--- viperz/src/App.js
+++ viperz/src/App.js
@@ -7,22 +7,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header whatever={'testing'} />
+      <Header/>
       <Banner/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
